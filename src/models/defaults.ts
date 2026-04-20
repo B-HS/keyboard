01984bec6f66f90f426265801c5cde03f32db5b5
Hyunseok Byun <gumyoincirno@gmail.com>
2026-04-20 09:32:25 +0900

    Adjust cutsize

--- src/models/defaults.ts
+++ src/models/defaults.ts
@@ -3,4 +3,17 @@ import { parseReference } from './reference'
 import { buildParamsFromReference } from './build-params'
 
 export const reference = parseReference(jscadSource)
-export const DEFAULT_BUILD_PARAMS = buildParamsFromReference(reference)
+
+const base = buildParamsFromReference(reference)
+
+export const DEFAULT_BUILD_PARAMS = {
+    ...base,
+    plate: {
+        ...base.plate,
+        switchCutoutSize: 13.95,
+        stabilizer: {
+            ...base.plate.stabilizer,
+            padSize: [6.9, 14.9] as [number, number],
+        },
+    },
+}
